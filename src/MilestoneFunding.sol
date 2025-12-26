5cf81434abb370fab760f55359356bc857907048
jimwu0516 <125996962+jimwu0516@users.noreply.github.com>
2025-12-26 23:09:15 +0800

    update version

--- src/MilestoneFunding.sol
+++ src/MilestoneFunding.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 contract MilestoneFunding is Ownable, ReentrancyGuard {
     constructor() Ownable(msg.sender) {}
